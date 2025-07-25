112875ff97736da7dbea68215d2380f1718c3a8a
robertWalker68501 <robert.walker6580@gmail.com>
2025-07-24 20:21:51 -0700

    fix prisma for vercel2

--- lib/auth-actions.ts
+++ lib/auth-actions.ts
@@ -3,9 +3,9 @@
 import { signIn, signOut } from '@/auth';
 
 export const login = async () => {
-  await signIn('github', { redirectTo: '/' });
+  await signIn('github', { redirectTo: '/https://travel-planner-egcjkr2zh-roberts-projects-2634047b.vercel.app/' });
 };
 
 export const logout = async () => {
-  await signOut({ redirectTo: '/' });
+  await signOut({ redirectTo: '/https://travel-planner-egcjkr2zh-roberts-projects-2634047b.vercel.app/' });
 };
